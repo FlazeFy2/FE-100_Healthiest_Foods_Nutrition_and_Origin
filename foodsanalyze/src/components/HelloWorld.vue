1b637f4518f58ba983d394282cdcfa76c53d674d
Leonardho Rante S <leonardho81@gmail.com>
2025-03-03 22:44:28 +0700

    [FE,DS] Descriptive Statistic for Calories, Protein, Fiber, Vitamin C, Antioxidant

--- foodsanalyze/src/components/HelloWorld.vue
+++ foodsanalyze/src/components/HelloWorld.vue
@@ -1,44 +1,52 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+  import A_TextComponent from "@/components/atoms/A_TextComponent.vue"
+  import M_DescriptiveStatistic from "@/components/molecules/M_DescriptiveStatistic.vue"
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <A_TextComponent title="100 Healthiest Foods Nutrition and Origin" />
+    <A_TextComponent second_title="About" />
+    <A_TextComponent content="The dataset offers a comprehensive view of consumer shopping trends, aiming to uncover patterns and behaviors in retail purchasing. It contains detailed transactional data across various product categories, customer demographics, and purchase channels"/>
+    <A_TextComponent second_title="Source" />
+    <a href="https://www.kaggle.com/datasets/prajwaldongre/top-100-healthiest-food-in-the-world" target="_blank" rel="noopener">Kaggle</a>
+    <a href="https://github.com/FlazeFy2/DS-100_Healthiest_Foods_Nutrition_and_Origin" target="_blank" rel="noopener">Jupiter Notebook</a>
+    
+    <hr>
+    <div class="d-flex justify-content-between">
+      <A_TextComponent second_title="Descriptive Statistic" />
+      <button class="btn btn-link rounded-pill" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDescriptiveStats" aria-expanded="false" aria-controls="collapseExample">Show Content</button>
+    </div>
+    <div class="collapse" id="collapseDescriptiveStats">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <M_DescriptiveStatistic target_col="Calories"/>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <M_DescriptiveStatistic target_col="Protein (g)"/>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <M_DescriptiveStatistic target_col="Fiber (g)"/>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <M_DescriptiveStatistic target_col="Vitamin C (mg)"/>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+          <M_DescriptiveStatistic target_col="Antioxidant Score"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+  a {
+    color: #42b883;
+    font-weight: 500;
+    font-size: 14px;
+    background-color:rgba(66, 184, 131, 0.25);
+    border-radius: 20px;
+    padding: 7px 12px;
+    margin-right: 6px;
   }
-}
 </style>
